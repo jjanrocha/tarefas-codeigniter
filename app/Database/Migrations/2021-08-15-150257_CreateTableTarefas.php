a15c84444b45c020e5bce5cec16d4b54b087a5e9
Janderson <janderson.rocha97@gmail.com>
2021-08-15 12:41:36 -0300

    Migration Create Table Tarefas

---
+++ app/Database/Migrations/2021-08-15-150257_CreateTableTarefas.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTableTarefas extends Migration
+{
+	public function up()
+	{
+		//criação do id da tabela
+		//há uma exceção para a criação de campos id, automaticamente são definidos como int(g), auto_increment e primary key
+		$this->forge->addField('id');
+
+		//criação dos demais campos da tabela tarefas
+		$this->forge->addField([
+			'tarefa' => [
+				'type' => 'VARCHAR',
+				'constraint' => 120
+			],
+			'data_limite_conclusao' => [
+				'type' => 'datetime'
+			],
+			'created_at datetime default current_timestamp',
+            'updated_at datetime default current_timestamp on update current_timestamp',
+			'delete_at datetime'
+		]);
+
+		//criação da tabela tarefas
+		$this->forge->createTable('tarefas');
+
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('tarefas');
+	}
+}
